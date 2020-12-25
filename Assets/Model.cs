fd7b7887d8479329f274f0deb592e95351da1990
Lukas Rambold <lukas@rambold.io>
2020-12-25 19:07:09 +0100

    initial commit

---
+++ Assets/Model.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Edge {
+	public int id;
+	public int v_src;
+	public int v_dest;
+}
+
+public struct Vertex {
+	public int id;
+	public Vector3 pos;
+} 
+
+public struct Graph {
+	public readonly Vertex[] vertices;
+	public readonly Edge[] edges;
+
+	public Graph(Vertex[] vertices, Edge[] edges) {
+		this.vertices = vertices;
+		this.edges = edges;
+	}
+
+	public static Graph getTetrahedron() {
+		return new Graph (
+    		 new Vertex[] {
+    			new Vertex { id = 0, pos = new Vector3(0,0,0) },
+    			new Vertex { id = 1, pos = new Vector3(0,1,0) },
+    			new Vertex { id = 2, pos = new Vector3(1,0,0) },
+    			new Vertex { id = 3, pos = new Vector3(0.5f,0.5f,0.5f) },
+    		},
+    		new Edge[] {
+    			new Edge { id = 0, v_src = 0, v_dest = 1 },
+    			new Edge { id = 1, v_src = 0, v_dest = 2 },
+    			new Edge { id = 2, v_src = 0, v_dest = 3 },
+    			new Edge { id = 3, v_src = 1, v_dest = 2 },
+    			new Edge { id = 4, v_src = 1, v_dest = 3 },
+    			new Edge { id = 5, v_src = 2, v_dest = 3 }
+    		}
+    	);
+	}
+
+	public bool[,] getAdjecencyMatrix () {
+		bool[,] result = new bool[this.vertices.Length, this.vertices.Length];
+		foreach (Edge e in this.edges){
+			result[e.v_dest, e.v_src] = true;
+			result[e.v_src, e.v_dest] = true;
+		}
+		return result;
+	}
+
+	// TODO return List instead
+	public List<int[]> getTriangles () {
+		List<int[]> triangles = new List<int[]>();
+		bool[,] adjMatrix = getAdjecencyMatrix();
+		Debug.Log(this.edges.Length);
+		for (int i = 0; i < this.edges.Length - 1; i++){
+			for (int j = i + 1; j < this.edges.Length; j++){
+				// if there are two edges that meet in one point, we are checkin whether the other ends on these edges are connected.
+				// if that is the case, we found a triangle
+				if (this.edges[i].v_src == this.edges[j].v_src && 
+					adjMatrix[this.edges[i].v_dest, this.edges[j].v_dest]) {
+					triangles.Add(new int[] {this.edges[i].v_src, this.edges[i].v_dest, this.edges[j].v_dest});
+				} else if (this.edges[i].v_src == this.edges[j].v_dest &&
+					 adjMatrix[this.edges[i].v_src, this.edges[j].v_dest]) {
+					triangles.Add(new int[] {this.edges[i].v_src, this.edges[i].v_dest, this.edges[j].v_src});
+				}
+			}
+		}
+		return triangles;
+	}
+}
+
+public class Model : MonoBehaviour
+{
+	private Graph graph;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// initialize Model with some dummy data (tetrahedron)
+    	this.graph = Graph.getTetrahedron();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Graph getGraph() {
+    	return graph;
+    }
+}
---
+++ Assets/View.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour
+{
+
+    private Model model;
+    private Graph currentlyActiveGraph;
+
+    public GameObject vertexPrefab;
+    public GameObject edgePrefab;
+    public GameObject buildingColliderPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        model = GameObject.Find("Model").GetComponent<Model>(); 
+        updateTrussStructure();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // updateTrussStructure();
+    }
+
+
+    void updateTrussStructure () {
+        Graph graph = model.getGraph();
+
+       // if (currentlyActiveGraph.Equals(graph)) {
+       //  // already up-to-date
+       //  return;
+       // }
+       
+       clearAllChildren();
+
+        for (int i = 0; i < graph.edges.Length; i++) {
+            drawTruss(graph.vertices[graph.edges[i].v_src].pos, graph.vertices[graph.edges[i].v_dest].pos);
+        }
+
+        for (int i = 0; i < graph.vertices.Length; i++) {
+            drawVertex(graph.vertices[i].pos);
+        }
+
+        this.drawCollisionBody();
+        currentlyActiveGraph = graph;
+    }
+
+    void drawCollisionBody() {
+        // find triangles
+        Graph graph = model.getGraph();
+        List<int []> triangles = graph.getTriangles();
+
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[graph.vertices.Length];
+        for (int i = 0; i < graph.vertices.Length; i++) {
+            vertices[i] = graph.vertices[i].pos;
+        }
+        mesh.vertices = vertices;
+
+        int[] tris = new int[3*triangles.Count];
+        for (int i = 0; i < triangles.Count; i++){
+            tris[i*3 + 0] = triangles[i][0];
+            tris[i*3 + 1] = triangles[i][1];
+            tris[i*3 + 2] = triangles[i][2];
+        }
+        mesh.triangles = tris;
+
+        meshFilter.mesh = mesh;
+    }
+
+    // void drawSelectionTriangles(Vector3[] triangleCorners) {
+    //     gameObject = new GameObject();
+    //     Instantiate(gameObject, transform.position, Quaternion.identity);
+    // }
+ 
+    void drawVertex(Vector3 pos) {
+        GameObject obj = (GameObject) Instantiate(vertexPrefab, pos, Quaternion.identity);
+        obj.transform.SetParent(this.transform);
+    }
+
+    void drawTruss(Vector3 p1, Vector3 p2) {
+        Vector3 pos = Vector3.Lerp(p1,p2,(float)0.5);
+        GameObject obj = (GameObject) Instantiate(edgePrefab, pos, Quaternion.identity);
+
+        obj.transform.SetParent(this.transform);
+        Vector3 newScale = obj.transform.localScale;
+        newScale.y = Vector3.Distance(p1,p2)/2;
+        obj.transform.localScale = newScale;
+        obj.transform.localRotation = Quaternion.FromToRotation(Vector3.up, p1 - p2);
+    }
+
+    void clearAllChildren() {
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+         }
+    }
+}
